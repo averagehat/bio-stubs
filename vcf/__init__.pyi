1de5ded7ab610cf69af18add5a69c7589a5247a0
Mike Panciera <bigmwp@gmail.com>
2020-01-23 19:57:55 -0500

    inital commit: some of pyvcf and BioSeq*

---
+++ vcf/__init__.pyi
@@ -0,0 +1,4 @@
+from vcf.parser import RESERVED_FORMAT as RESERVED_FORMAT, RESERVED_INFO as RESERVED_INFO, Reader as Reader, VCFReader as VCFReader, VCFWriter as VCFWriter, Writer as Writer
+from vcf.sample_filter import SampleFilter as SampleFilter
+
+VERSION: str
